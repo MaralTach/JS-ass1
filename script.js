fe00f2654a630a0639d17de5b4392bcfcff06b39
MaralTach <yoloten.maral@gmail.com>
2024-02-22 17:49:32 +0300

    js-ass1-starts

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const mainContainer = document.querySelector('body');
+
+console.log(mainContainer);
+mainContainer.style.backgroundColor = 'bisque'
+
+
+const headText = document.getElementById('title');
+
+console.log(headText);
+headText.style.color ='yellow';
+headText.style.backgroundColor ='green' ;
+headText.style.border = '2px solid red';
+headText.style.borderRadius = '20px ';
+headText.style.padding = '20px ';
+headText.style.textAlign = 'center';
+// headText.style.position = 'fixed';
+headText.style.width = '100%';
+
+
+const navbar = document.getElementsByClassName('nav-item');
+console.log(navbar);
+
+navbar.style.color = 'orange'
+
+
+
